b7d285e150f19e9471e516a61b0787742e536645
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-07 16:46:23 +0500

    added a few files

---
+++ task4.py
@@ -0,0 +1,12 @@
+"""
+    Task no: Task 4
+    Description: A program thats asks a user for 2 numbers then
+    prints the sum
+    Return: no return 
+"""
+
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+
+sum = num1 + num2
+print(f"The total is {sum}")
