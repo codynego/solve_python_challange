c6e1fa7767807361c033e50b623756bf451b7f22
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-07 16:34:19 +0500

    added task2 file

---
+++ task2.py
@@ -0,0 +1,11 @@
+"""
+    Task no: Task 2
+    Description: A program thats asks for a users first name and lastname
+    then prints the output
+    Return: no return 
+"""
+
+first_name = input("enter your first name: ")
+last_name = input("enter your last name: ")
+
+print(f"Hello {first_name} {last_name} ")
