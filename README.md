cdce0ac62c4b83bfc54fcdf812ee836b74b203fd
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-07 16:30:42 +0500

    added a few files

---
+++ README.md
@@ -0,0 +1,2 @@
+In my journey to learn about python,
+this repo contains tasks, challenge and project that will help me learn python and its syntax
---
+++ task1.py
@@ -0,0 +1,8 @@
+"""
+    Task no: Task 1
+    Description: A program thats asks for a users first name
+    then prints the output
+    Return: no return 
+"""
+first_name = input("what's your first name? > ")
+print(f"Hello {first_name} ")
