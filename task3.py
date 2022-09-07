072a4df258db3736f0959010a2bb978c9bfa1049
Abednego Emonena <emonenaabednego@gmail.com>
2022-09-07 16:38:29 +0500

    added task3 file

---
+++ task3.py
@@ -0,0 +1,8 @@
+"""
+    Task no: Task 3
+    Description: A program thats prints 2 sentence in 2 different lines
+    in one line of code
+    Return: no return 
+"""
+
+print("What do you call a bear with no teeth?\nAnswer: A gummy bear!")
